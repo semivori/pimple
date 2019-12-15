70bed569d9ce5f9465773f9fde42752f69eb7b73
semivori <semivoriii@gmail.com>
2019-12-15 02:04:48 +0200

    test 2

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ class Curl
     public function exec()
     {
         if ($this->url) {
+            usleep(500000);
             $curl = curl_init($this->url);
             curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36");
             curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
