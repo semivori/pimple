1ecf39f420c4db60793f4c2afa85ec509e9ae1f4
semivori <semivoriii@gmail.com>
2019-12-15 04:18:06 +0200

    front

---
+++ index.js
@@ -0,0 +1,53 @@
+// Try edit message
+
+
+const getData = async () => {
+    const res = await fetch('https://sv-fas.herokuapp.com');
+    return await res.json();
+};
+
+const copyToClipboard = {
+    click: function (e) {
+        const input = document.createElement('input');
+        input.value = e.currentTarget.dataset.copy;
+        console.log(e.target.dataset.copy);
+        document.body.append(input);
+        input.select();
+        document.execCommand("copy");
+        input.remove();
+
+    }
+};
+
+const addDataToTable = (data) => {
+    const table = document.getElementById('js__pimple__matches-table');
+    const spinners = document.getElementById('js__pimple__matches_spinners-div');
+    table.classList.remove("d-none");
+    spinners.classList.add("d-none");
+
+    data.forEach(function (item, key) {
+        let row = table.insertRow(key);
+        let cellName = row.insertCell(0);
+        let cellLink = row.insertCell(1);
+        let cellBtn = row.insertCell(2);
+        let btn = document.createElement('button');
+        btn.type = 'button';
+        btn.classList.add('btn', 'btn-secondary', 'btn-sm', 'js__copy_btn');
+        btn.dataset.copy = item.acestream;
+        btn.innerText = 'Copy';
+        btn.addEventListener('click', copyToClipboard.click);
+
+
+        cellName.innerHTML = item.name;
+        cellLink.innerHTML = item.acestream;
+        cellBtn.append(btn);
+    });
+};
+
+const init = () => {
+    getData().then(function (data) {
+        addDataToTable(data);
+    });
+};
+
+init();
